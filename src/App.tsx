9c5da1da0a682972dbbeb3479b8a5d89202a85f7
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-03-15 17:19:10 -0300

    get informations by country name

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,43 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Weather} from './Components/Weather';
 
+
+type City = {
+  country: string;
+  lat: number;
+  lon: number;
+}
+
 function App() {
 
-  const [weatherData, setWeatherData] = useState<[]>([]);
+  const [city, setCity] = useState<City | null>(null);
 
   const api_key = "2a13aa0a341607040488e8501fc48eb5";
-  const city_name = "London";
-  const api = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=${api_key}`
+  const city_name = "amsterdan";
+  const api = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=1&appid=${api_key}`
 
   useEffect(() => {
-      axios.get(api)
-          .then(response => {
-          setWeatherData(response.data);
-          console.log(weatherData)
-          })
-          .catch(error => {
-          console.log(error);
-          })
-  }, [])
+    const getData = async () => {
+      try{
+        const response = await axios.get(api);
+        setCity(response.data[0]);
+      }catch (error){
+        console.log(error)
+      }
+    }
+
+    getData();
+  }, [api])
+    
+  
   return (
     <div className="App">
-      {weatherData.map((item: {name: string, lat:number, lon: number}) =>(
-          <div>
-              <Weather lat={item.lat}  lon={item.lon}/>
-          </div>
-      ))}
-    </div>
+    {city ? (
+      <Weather lat={city.lat} lon={city.lon} />
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
   )
 }
 
--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -1,16 +1,60 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 interface WeatherProps {
     lat: number,
     lon:number,
-  }
+}
 
-export const Weather = (props:WeatherProps): JSX.Element => {
+export const Weather = (props: WeatherProps): JSX.Element => {
 
-    // example = https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+    const [weather, setWeather] = useState<null | any>(null);
 
-    return(
+    const api_key = "2a13aa0a341607040488e8501fc48eb5";
+    // const lat = 50.84238515061159;
+    // const lon =  4.362321252794411;
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.lon}&appid=${api_key}`;
+
+    // const getData = async () => {
+    //     try{
+    //         const response  = await axios.get(api);
+    //         console.log(api);
+    //         setWeather(response.data);
+    //         console.log(weather);
+    //     }catch(error){
+    //         console.log(error);
+    //     }
+    // }
+    
+    useEffect(() => {
+        const getData = async () => {
+          try{
+            const response = await axios.get(api);
+            setWeather(response.data);
+            console.log(api)
+          }catch (error){
+            console.log(error)
+          }
+        }
+    
+        getData();
+      }, [api])
+
+    return(    
         <div>
-            <h1>Latitude: {props.lat}</h1>
-            <h1>Longitude: {props.lon}</h1>
+            {weather ? (
+                <ul>
+                    <li>Lat: {weather.coord.lat}</li>
+                    <li>Lon: {weather.coord.lon}</li>
+                    <li>Name: {weather.name}</li>
+                    <li>{weather.main.temp}</li>
+                    <li>{weather.sys.country}</li>
+                </ul>
+            ): (
+                <p>Loading...</p>
+            )}
+            
         </div>
+            
     )
 }
